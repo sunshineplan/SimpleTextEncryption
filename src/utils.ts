db29885d916fa8902829f60aec25a656a6752529
sunshineplan <sunshineplan@users.noreply.github.com>
2025-02-12 17:07:53 +0800

    Use GCM instead of CCM

--- src/utils.ts
+++ src/utils.ts
@@ -1,31 +1,17 @@
-import sjcl from 'sjcl'
 import pako from 'pako'
 
 namespace utils {
   export const compress = (str: string) => {
-    const uint8array = new TextEncoder().encode(str)
+    const encoder = new TextEncoder()
+    const uint8array = encoder.encode(str)
     const deflate = pako.deflate(uint8array)
     if (uint8array.length > deflate.length)
-      return { content: Uint8ArrayToBits(deflate), compression: sjcl.codec.utf8String.toBits('1') }
-    else return { content: Uint8ArrayToBits(uint8array), compression: sjcl.codec.utf8String.toBits('0') }
+      return { content: deflate, compression: 1 }
+    else return { content: uint8array, compression: 0 }
   }
 
-  export const decompress = (bits: sjcl.BitArray) => {
-    return pako.inflate(BitsToUint8Array(bits), { to: 'string' })
-  }
-
-  export const Uint8ArrayToBits = (uint8array: Uint8Array) => {
-    let hex = ''
-    for (let i = 0; i < uint8array.length; i++)
-      hex += (uint8array[i] + 0xF00).toString(16).slice(1)
-    return sjcl.codec.hex.toBits(hex)
-  }
-
-  export const BitsToUint8Array = (bits: sjcl.BitArray) => {
-    const array: number[] = [], hex = sjcl.codec.hex.fromBits(bits)
-    for (var i = 0; i < hex.length; i += 2)
-      array.push(parseInt(hex.slice(i, i + 2), 16))
-    return new Uint8Array(array)
+  export const decompress = (data: ArrayBuffer) => {
+    return pako.inflate(data, { to: 'string' })
   }
 }
 
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,26 +1,17 @@
-import crypto from 'crypto'
-import sjcl from 'sjcl'
 import { expect, test } from 'vitest'
 import utils from '../src/utils'
 
 test('Compress&Decompress', () => {
   for (let i = 0; i < 100; i++) {
-    const str = crypto.randomBytes(Math.floor((Math.random() * 10) + 1)).toString('hex')
+    const str = Buffer.from(crypto.getRandomValues(new Uint8Array((Math.random() * 10) + 1))).toString('hex')
     const c = utils.compress(str)
-    expect(c.compression).toStrictEqual(sjcl.codec.utf8String.toBits('0'))
-    expect(c.content).toStrictEqual(sjcl.codec.utf8String.toBits(str))
+    expect(c.compression).toStrictEqual(0)
+    expect(Buffer.from(c.content).toString()).toStrictEqual(str)
   }
   for (let i = 0; i < 100; i++) {
-    const str = crypto.randomBytes(100 + (Math.random() * 100) + 1).toString('hex')
+    const str = Buffer.from(crypto.getRandomValues(new Uint8Array((100 + (Math.random() * 100) + 1)))).toString('hex')
     const c = utils.compress(str)
-    expect(c.compression).toStrictEqual(sjcl.codec.utf8String.toBits('1'))
+    expect(c.compression).toStrictEqual(1)
     expect(utils.decompress(c.content)).toBe(str)
   }
-})
-
-test('ConvertUint8Array&Bits', () => {
-  const bits = sjcl.codec.utf8String.toBits('abc')
-  const array = new Uint8Array([97, 98, 99])
-  expect(utils.Uint8ArrayToBits(array)).toStrictEqual(bits)
-  expect(utils.BitsToUint8Array(bits)).toStrictEqual(array)
 })
